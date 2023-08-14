c312fdbc24d1a56982de60584209706eeb0054f2
Yaroslav Lizogub <lizogub.yar@gmail.com>
2023-08-14 14:04:36 +0300

    webhook test

--- api/webhook.js
+++ api/webhook.js
@@ -2,6 +2,7 @@
 // We'll wait until we've responded to the user
 const TelegramBot = require('node-telegram-bot-api');
 
+process.env.NTBA_FIX_319 = 'test';
 module.exports = async (request, response) => {
 
     try {
